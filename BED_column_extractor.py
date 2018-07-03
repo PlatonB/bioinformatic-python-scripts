b8b224f96552af5a5090b179269ed0cabe63c249
PlatonB <platon.work@gmail.com>
2018-07-03 21:04:44 +0300

    Ещё одна переработка экстрактора столбцов
    Главное изменение: независимо от размера исходных файлов, скрипт практически не будет задействовать RAM. Остальное: добавлен пример исходной и конечной таблицы (теперь буду стараться внедрять подобные примеры в каждую программу), и, как обычно, улучшены имена переменных.

--- BED_column_extractor.py
+++ BED_column_extractor.py
@@ -1,47 +1,52 @@
 print('''
-Этот Python3-скрипт копирует в отдельные файлы необходимые пользователю столбцы BED-таблиц.
+Этот Python3-скрипт копирует в отдельные файлы необходимые пользователю столбцы.
 Автор: Платон Быкадоров, 2017-2018.
 Лицензия: GNU General Public License version 3.
 Поддержать проект: https://money.yandex.ru/to/41001832285976
 
-Формат BED:
-
-#Заголовок со знаком решётки в начале, либо такого вида:
-track name="Заголовок" description="Заголовок по стандатрам UCSC Table Browser. Любые заголовки BED-таблиц для этого скрипта необязательны" visibility=3 url=
-chr6 (или 6)	32665241	32665242	(опционально) дополнительные поля, разделённые табуляциями
-chr6 (или 6)	32665244	32665245	(опционально) дополнительные поля, разделённые табуляциями
-...
-
-В исходной папке не должно находиться ничего, кроме BED-файлов, с которыми будет работать скрипт.
+Пример.
+Первые 7 столбцов таблицы аннотаций трёх SNP:
+rs12896956	14:105767258-105767258	T	intron_variant	MODIFIER	IGHG3	ENSG00000211897
+rs12897751	14:105767411-105767411	C	intron_variant	MODIFIER	IGHG3	ENSG00000211897
+rs3020575	14:105760146-105760146	A	downstream_gene_variant	MODIFIER	IGHG3	ENSG00000211897
+Оставляем только столбцы с refSNPID и Official Gene Symbol (1-й и 6-й):
+rs12896956	IGHG3
+rs12897751	IGHG3
+rs3020575	IGHG3
 ''')
 
-source_dir = input('Путь к папке с исходными файлами: ')
-columns_numbers = input('Номер одного столбца или номера нескольких столбцов через пробел: ').split()
-target_dir = input('Путь к папке с конечными файлами: ')
+src_dir_path = input('Путь к папке с исходными файлами: ')
+trg_dir_path = input('Путь к папке для конечных файлов: ')
+col_numbers = [int(col_number) for col_number in input('''Номер одного или номера нескольких экстрагируемых столбцов
+(через пробел): ''').split()]
+num_of_headers = int(input('''Количество не считываемых строк в начале файла
+(совет: табулированную шапку к ним не причисляйте) [0|1|2|(...)]: '''))
 
 import os
-import csv
 
-source_files = os.listdir(source_dir)
-for source_file in source_files:
-        with open(os.path.join(source_dir, source_file)) as sf_opened:
+src_file_names = os.listdir(src_dir_path)
+for src_file_name in src_file_names:
+        with open(os.path.join(src_dir_path, src_file_name)) as src_file_opened:
 
-                #Создание двумерного массива по данным из исходной таблицы.
-                two_dim = list(csv.reader(sf_opened, delimiter = '\t'))
+                #Конструируемое ниже имя конечного файла будет содержать номера выделенных столбцов.
+                trg_file_name = src_file_name.split('.')[0] + '_col' + \
+                                '+'.join([str(col_number) for col_number in col_numbers]) + '.txt'
+                with open(os.path.join(trg_dir_path, trg_file_name), 'w') as trg_file_opened:
 
-                #Обработка хэдэра.
-                if two_dim[0][0].find('#') != -1 or two_dim[0][0].find('track name=') != -1:
-                        header = two_dim[0][0]
-                else:
-                        print('Ошибка. В файле ' + source_file + ' отсутствует или неправильно оформлен хэдер.')
-                        break
-
-                #Создание конечного двумерного массива, содержащего только запрашиваемые "столбцы".
-                trun_two_dim = [[row[int(column_number) - 1] for column_number in columns_numbers] for row in two_dim[1:]]
+                        #Формирование списка хэдеров и добавление хэдеров в конечный файл.
+                        #Курсор смещается к началу основной части таблицы.
+                        headers = [src_file_opened.readline() for header_index in range(num_of_headers)]
+                        for header in headers:
+                                trg_file_opened.write(header)
                                 
-                #Формирование конечного файла.
-                target_file = source_file.split('.')[0] + '_' + 'col' + '_'.join(columns_numbers) + '.txt'
-                with open(os.path.join(target_dir, target_file), 'w') as tf_opened:
-                        tf_opened.write(header + '\n')
-                        for row in trun_two_dim:
-                                tf_opened.write('\t'.join(row) + '\n')
+                        #Эффективная с точки зрения использования RAM
+                        #работа с основной частью таблицы.
+                        for line in src_file_opened:
+                                src_row = line.split('\t')
+
+                                #Формирование списка, содержащего "ячейки" только запрашиваемых "столбцов".
+                                trg_row = [src_row[col_number - 1] for col_number in col_numbers]
+
+                                if trg_row[-1].find('\n') == -1:
+                                        trg_row[-1] += '\n'
+                                trg_file_opened.write('\t'.join(trg_row))
