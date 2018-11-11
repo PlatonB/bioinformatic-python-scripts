1d526f7d325f5faa1b2dab72cecc09066c569f68
PlatonB <platon.work@gmail.com>
2018-11-11 23:13:06 +0300

    Предпросмотрщик больших файлов, V2.2
    - Защита от неправильного написания одной из опций.
    - Обработка имён файлов, в которых помимо точки перед расширением есть и другие точки.

--- Large_table_previewer.py
+++ Large_table_previewer.py
@@ -1,7 +1,7 @@
 print('''
 Python3-скрипт, выводящий первые n строк большого текстового файла.
 Автор: Платон Быкадоров (platon.work@gmail.com), 2018.
-Версия: V2.1.
+Версия: V2.2.
 Лицензия: GNU General Public License version 3.
 Поддержать проект: https://money.yandex.ru/to/41001832285976
 
@@ -19,17 +19,21 @@ github.com/PlatonB/bioinformatic-python-scripts#Установка-среды-р
 консоль, в которой вы запускаете скрипт, может зависнуть.
 ''')
 
-import os
+import sys, os
 
 src_file_path = input('Путь к исходному файлу: ')
 str_quan = int(input('\nКоличество выводимых или перенаправляемых строк: '))
 redirection = input('''\nПеренаправить результат в отдельный файл?
 (игнорирование ввода ==> вывести результат на экран)
 [redirect|<enter>]: ''')
+if redirection != 'redirect' and redirection != '':
+        print(f'{redirection} - недопустимая опция')
+        sys.exit()
 with open(src_file_path) as src_file_opened:
         if redirection == 'redirect':
-                trg_dir_path = input('Путь к папке для конечного файла: ')
-                trg_file_name = os.path.basename(src_file_path).split('.')[0] + '_fir_' + str(str_quan) + '.txt'
+                trg_dir_path = input('\nПуть к папке для конечного файла: ')
+                '.'.join(os.path.basename(src_file_path).split('.')[:-1])
+                trg_file_name = '.'.join(os.path.basename(src_file_path).split('.')[:-1]) + '_fir_' + str(str_quan) + '.txt'
                 with open(os.path.join(trg_dir_path, trg_file_name), 'w') as trg_file_opened:
                         for num in range(str_quan):
                                 line = src_file_opened.readline()
