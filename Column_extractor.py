2c28c0c716fb6cf03decc37baff51222d08018f2
PlatonB <platon.work@gmail.com>
2018-11-27 16:40:35 +0300

    Экстрактор столбцов, V2.4
    - Применён новый, и, надеюсь, окончательный стандарт построения имён конечных файлов. Любым таблицам присваивать .tsv было опрометчиво, т.к. терялись узкоспециализированные расширения вроде .bed и .vcf. Теперь у output-файлов будут те же расширения, что и у input.
    - Традиционно, немного усовершенствован диалог с пользователем.

--- Column_extractor.py
+++ Column_extractor.py
@@ -1,7 +1,7 @@
 print('''
 Этот Python3-скрипт копирует в отдельные файлы необходимые пользователю столбцы.
 Автор: Платон Быкадоров (platon.work@gmail.com), 2017-2018.
-Версия: V2.3.
+Версия: V2.4.
 Лицензия: GNU General Public License version 3.
 Поддержать проект: https://money.yandex.ru/to/41001832285976
 
@@ -25,7 +25,8 @@ import os
 
 src_dir_path = input('Путь к папке с исходными файлами: ')
 trg_dir_path = input('\nПуть к папке для конечных файлов: ')
-num_of_headers = input('''\nКоличество не обрабатываемых строк в начале файла
+num_of_headers = input('''\nКоличество не обрабатываемых строк
+в начале каждой исходной таблицы
 (совет: табулированную шапку к ним не причисляйте)
 (игнорирование ввода ==> производить работу для всех строк)
 [0(|<enter>)|1|2|...]: ''')
@@ -33,7 +34,8 @@ if num_of_headers == '':
         num_of_headers = 0
 else:
         num_of_headers = int(num_of_headers)
-col_numbers = [int(col_number) for col_number in input('''\nНомер одного или номера нескольких экстрагируемых столбцов
+col_numbers = [int(col_number) for col_number in input('''\nНомер одного или номера
+нескольких экстрагируемых столбцов
 (через пробел): ''').split()]
 
 src_file_names = os.listdir(src_dir_path)
@@ -43,8 +45,10 @@ for src_file_name in src_file_names:
                 
                 #Конструируемое ниже имя конечного файла
                 #будет содержать номера выделенных столбцов.
-                trg_file_name = '.'.join(src_file_name.split('.')[:-1]) + '_col' + \
-                                '+'.join([str(col_number) for col_number in col_numbers]) + '.tsv'
+                src_file_base = '.'.join(src_file_name.split('.')[:-1])
+                src_file_ext = src_file_name.split('.')[-1]
+                trg_file_name = src_file_base + '_col' + \
+                                '+'.join([str(col_number) for col_number in col_numbers]) + '.' + src_file_ext
                 
                 with open(os.path.join(trg_dir_path, trg_file_name), 'w') as trg_file_opened:
                         
