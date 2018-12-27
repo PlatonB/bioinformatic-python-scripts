f112baf68221433ed6dee3ff419c842ca316d69a
PlatonB <platon.work@gmail.com>
2018-12-28 02:58:30 +0300

    Программа, извлекающая сцепленные SNPs, V1.4
    - Возврат таких наработок из [ld_triangle](https://github.com/PlatonB/ld_triangle), как остановка выполнения скрипта при получении ответов 503 и 429, защита от неправильного ввода опций и вывод названий текущих исходных refSNPID-содержащих файлов.
    - Обработка ошибок, возвращаемых Энсемблом в виде словаря.
    - Значениями LD для пары "запрашиваемый-запрашиваемый" теперь будут не единички, а прочерки. Это может чуточку усложнять дальнейшую программную обработку результатов, но зато больше не будет абсурда: не может у снипа самого с собой по определению быть LD.

--- Linked_SNPs_search.py
+++ Linked_SNPs_search.py
@@ -3,7 +3,7 @@ print('''
 неравновесием по сцеплению с запрашиваемыми SNPs не ниже определённого
 порога r2 или D', и находящиеся в пределах "окна", равного 500 кбаз.
 Автор: Платон Быкадоров (platon.work@gmail.com), 2018.
-Версия: V1.3.
+Версия: V1.4.
 Лицензия: GNU General Public License version 3.
 Поддержать проект: https://money.yandex.ru/to/41001832285976
 
@@ -20,19 +20,30 @@ github.com/PlatonB/bioinformatic-python-scripts#Установка-среды-р
 понятны - пишите, пожалуйста, в Issues.
 ''')
 
-def query(url_template, refsnpid):
+def query_to_ens(url_template, refsnpid):
         '''
         Добавление текущего refSNPID в шаблон
         запроса и осуществление этого запроса.
+        Проверка, не выдал ли сервер наиболее популярные,
+        согласно моему опыту, ответы 503 и 429.
         Возврат списка словарей, содержащих пару refSNPIDs
         (запрашиваемый и найденный), название популяции,
         а также значения r2 и D' для данной пары.
         '''
         url = url_template.replace('refsnpid', refsnpid)
-        print('Обрабатывается запрос', url)
+        print(url)
         r = requests.get(url)
-        if not r.ok:
-                r.raise_for_status()
+        if r.status_code == 503:
+                print('''\n503
+Сервер временно не имеет возможности
+обрабатывать запросы по техническим причинам.
+Работа программы будет завершена.''')
+                sys.exit()
+        elif r.status_code == 429:
+                print('''\n429
+Вы попытались отправить слишком
+много запросов за короткое время.
+Работа программы будет завершена.''')
                 sys.exit()
         decoded = r.json()
         return decoded
@@ -43,8 +54,9 @@ import os, sys, requests, re, json
 
 src_dir_path = input('Путь к папке с исходными файлами: ')
 trg_top_dir_path = input('\nПуть к папке для папок с конечными файлами: ')
-num_of_headers = input('''\nКоличество не обрабатываемых строк в начале файла
-(игнорирование ввода ==> хэдеров/шапок в таблице нет)
+num_of_headers = input('''\nКоличество не обрабатываемых строк
+в начале каждой исходной таблицы
+(игнорирование ввода ==> хэдеров/шапок в таблицах нет)
 [0(|<enter>)|1|2|...]: ''')
 if num_of_headers == '':
         num_of_headers = 0
@@ -56,23 +68,39 @@ species = input('''\nВид
 [homo_sapiens(|<enter>)|panthera_pardus|pteropus_vampyrus|...]: ''')
 if species == '':
         species = 'homo_sapiens'
-pop_name = input('''\nПопуляция из 1000 Genomes
+pop_name = input('''\nПопуляция по 1000 Genomes
 (ensembl.org/info/genome/variation/species/populations.html)
-[1000GENOMES:phase_3:EUR|1000GENOMES:phase_3:ALL|...]: ''')
+(игнорирование ввода ==> 1000GENOMES:phase_3:ALL)
+[1000GENOMES:phase_3:ALL(|<enter>)|1000GENOMES:phase_3:EUR|...]: ''')
+if pop_name == '':
+        pop_name = '1000GENOMES:phase_3:ALL'
+elif pop_name.startswith('1000GENOMES:phase_3:') == False:
+        print(f'{pop_name} - недопустимая опция')
+        sys.exit()
 ld_measure = input('''\nМера неравновесия сцепления (LD)
 [r2|d_prime]: ''')
+if ld_measure != 'r2' and ld_measure != 'd_prime':
+        print(f'{ld_measure} - недопустимая опция')
+        sys.exit()
 ld_value = float(input('\n{} >= '.format(ld_measure)))
 query_snp_save = input('''\nВыводить в конечный файл запрашиваемый SNP?
 (игнорирование ввода ==> не выводить)
-[yes(|y)|<enter>]: ''')
+[yes(|y)|no(|n|<enter>)]: ''')
+if query_snp_save != 'yes' and query_snp_save != 'y' and query_snp_save != 'no' \
+   and query_snp_save != 'n' and query_snp_save != '':
+        print(f'{query_snp_save} - недопустимая опция')
+        sys.exit()
 trg_file_format = input('''\nФормат конечных файлов
 (игнорирование ввода ==> json)
 [json(|<enter>)|tsv]: ''')
+if trg_file_format != 'json' and trg_file_format != 'tsv' and trg_file_format != '':
+        print(f'{trg_file_format} - недопустимая опция')
+        sys.exit()
 if trg_file_format == '':
         trg_file_format = 'json'
         
 #Первичный шаблон запроса к API и его заполнение
-#общими для всех запрашиваемых SNPs данными.
+#общими для всех запрашиваемых SNP данными.
 emp_url_templ = 'https://rest.ensembl.org/ld/{}/refsnpid/{}?content-type=application/json;{}={}'
 fil_url_templ = emp_url_templ.format(species, pop_name, ld_measure, ld_value)
 
@@ -90,6 +118,9 @@ for src_file_name in src_file_names:
                 #курсор к началу основной части таблицы.
                 for header_index in range(num_of_headers):
                         src_file_opened.readline()
+                        
+                print(f'''\n\tПолучение SNP, неравновесно сцепленных с каждым SNP из файла
+\t\t{src_file_name}''')
                 
                 #Считывание строк основной части таблицы.
                 for line in src_file_opened:
@@ -112,11 +143,17 @@ for src_file_name in src_file_names:
                         
                         #Запрос к Ensembl REST API, получение
                         #списка словарей с выходными данными.
-                        linked_snps_n_specs = query(fil_url_templ, rs_id)
+                        linked_snps_n_specs = query_to_ens(fil_url_templ, rs_id)
                         
                         #Если полученный список - пустой, то соответствующий
                         #ему конечный файл создаваться не будет.
+                        #Если вместо списка выдаётся словарь, то этот
+                        #словарь - исключительно с сообщением об ошибке.
+                        #В обоих случаях переходим к следующей строке.
                         if linked_snps_n_specs == []:
+                                continue
+                        if type(linked_snps_n_specs).__name__ == 'dict':
+                                print(rs_id, '- невалидный идентификатор')
                                 continue
                         
                         #Опциональное дополнение списка словарей элементом,
@@ -125,8 +162,8 @@ for src_file_name in src_file_names:
                         if query_snp_save == 'yes' or query_snp_save == 'y':
                                 query_snp_n_specs = {'variation1': rs_id,
                                                      'variation2': rs_id,
-                                                     'r2': '1.000000',
-                                                     'd_prime': '1.000000',
+                                                     'r2': '-',
+                                                     'd_prime': '-',
                                                      'population_name': pop_name}
                                 linked_snps_n_specs.insert(0, query_snp_n_specs)
                                 
