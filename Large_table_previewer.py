ab4c753528263ad8b7a8e0e6e0ded788373b15a4
PlatonB <platon.work@gmail.com>
2019-06-09 20:01:21 +0300

    Предпросмотрщик больших файлов, V3.0
    - Поддержка вывода текста из GZIP/BGZIP-архивов.

--- Large_table_previewer.py
+++ Large_table_previewer.py
@@ -1,7 +1,7 @@
 print('''
 Python3-скрипт, выводящий первые n строк большого текстового файла.
-Автор: Платон Быкадоров (platon.work@gmail.com), 2018.
-Версия: V2.2.
+Автор: Платон Быкадоров (platon.work@gmail.com), 2018-2019.
+Версия: V3.0.
 Лицензия: GNU General Public License version 3.
 Поддержать проект: https://money.yandex.ru/to/41001832285976
 
@@ -15,31 +15,49 @@ github.com/PlatonB/bioinformatic-python-scripts#Установка-среды-р
 Ознакомление с общим видом таблицы сделает возможным
 создавать специализированные программы для её обработки.
 
+Поддерживаются текстовые файлы в обычном и сжатом ((b)gzip) виде.
+
 Не выводите не экран слишком много строк (тысячи), т.к.
 консоль, в которой вы запускаете скрипт, может зависнуть.
 ''')
 
-import sys, os
+import sys, re, gzip, os
 
-src_file_path = input('Путь к исходному файлу: ')
+src_file_path = input('Путь к исходному файлу (текст или gz/bgz-архив): ')
+
 str_quan = int(input('\nКоличество выводимых или перенаправляемых строк: '))
+
 redirection = input('''\nПеренаправить результат в отдельный файл?
 (игнорирование ввода ==> вывести результат на экран)
-[redirect|<enter>]: ''')
-if redirection != 'redirect' and redirection != '':
+[redirect(|r)|<enter>]: ''')
+if redirection not in ['redirect', 'r', '']:
         print(f'{redirection} - недопустимая опция')
         sys.exit()
-with open(src_file_path) as src_file_opened:
-        if redirection == 'redirect':
-                trg_dir_path = input('\nПуть к папке для конечного файла: ')
-                trg_file_name = '.'.join(os.path.basename(src_file_path).split('.')[:-1]) + '_fir_' + str(str_quan) + '.txt'
-                with open(os.path.join(trg_dir_path, trg_file_name), 'w') as trg_file_opened:
-                        for num in range(str_quan):
-                                line = src_file_opened.readline()
-                                if line.find('\n') == -1:
-                                        line += '\n'
-                                trg_file_opened.write(line)
-        else:
+
+#Открытие на чтение (b)gzip-архива.
+if re.search(r'\.b?gz$', src_file_path) != None:
+        src_file_opened = gzip.open(src_file_path, mode='rt')
+        
+#Открытие на чтение текстового файла.
+else:
+        src_file_opened = open(src_file_path)
+        
+#Выбрано перенаправление в файл.
+if redirection in ['redirect', 'r']:
+        trg_dir_path = input('\nПуть к папке для конечного файла: ')
+        trg_file_name = '.'.join(os.path.basename(src_file_path).split('.')[:-1]) + '_fir_' + str(str_quan) + '.txt'
+        with open(os.path.join(trg_dir_path, trg_file_name), 'w') as trg_file_opened:
                 for num in range(str_quan):
-                        line = src_file_opened.readline().split('\n')[0]
-                        print(line)
+                        line = src_file_opened.readline()
+                        if line.find('\n') == -1:
+                                line += '\n'
+                        trg_file_opened.write(line)
+                        
+#Выбран вывод на экран.
+else:
+        print('')
+        for num in range(str_quan):
+                line = src_file_opened.readline().split('\n')[0]
+                print(line)
+                
+src_file_opened.close()
